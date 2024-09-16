a99dbda6a73bc6e1d81ceb05ddb3031cc7c0041d
Sulabh <sulabhambule@gmail.com>
2024-09-16 18:46:34 +0530

    adddddddddddddd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5173",
+      "/api": "https://sign-in-frontend-delta.vercel.app",
     },
   },
   plugins: [react()],
